d2dd99982d92ad5d2543bdcbc3875278f847e39a
Daeho <dooho199234@nate.com>
2021-06-04 20:45:38 +0900

    procject init

---
+++ src/main/index.js
@@ -0,0 +1,66 @@
+import "./index.css";
+import axios from "axios";
+import React from "react";
+
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://7d61b592-d0b4-4724-b883-e38d1b8ec9df.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 :", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" />
+        </div>
+      </div>
+
+      <div id="body">
+        <div id="banner">
+          <img src="images/banners/banner1.png" />
+        </div>
+
+        <h1>판매되는 상품들</h1>
+
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div id="footer"></div>
+    </div>
+  );
+}
+
+export default MainPage;
