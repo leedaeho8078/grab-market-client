a7bdc25bf1cb03614d717c341fb969a16c9c15c1
Daeho <dooho199234@nate.com>
2021-06-11 15:03:04 +0900

    반응형 수정

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,11 +1,50 @@
-import { Divider, Form, Input, InputNumber, Button } from "antd";
+import {
+  Divider,
+  Form,
+  Input,
+  InputNumber,
+  Button,
+  Upload,
+  message,
+} from "antd";
+import axios from "axios";
+import { useState } from "react";
+import { API_URL } from "../config/constants";
 import "./index.css";
+import { useHistory } from "react-router-dom";
 
 function UploadPage() {
-  const onSubmit = (values) => {
-    console.log(values);
-  };
+  const [imageUrl, setImageUrl] = useState(null);
+  const history = useHistory();
 
+  const onSubmit = (values) => {
+    axios
+      .post(`${API_URL}/products`, {
+        name: values.name,
+        description: values.description,
+        seller: values.seller,
+        price: parseInt(values.price),
+        imageUrl: imageUrl,
+      })
+      .then((result) => {
+        console.log(result);
+        history.replace("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
+  const onChangeImage = (info) => {
+    if (info.file.status === "uploading") {
+      return;
+    }
+    if (info.file.status === "done") {
+      const response = info.file.response;
+      const imageUrl = response.imageUrl;
+      setImageUrl(imageUrl);
+    }
+  };
   return (
     <div id="upload-container">
       <Form name="상품 업로드" onFinish={onSubmit}>
@@ -13,10 +52,22 @@ function UploadPage() {
           name="upload"
           label={<div className="upload-label">상품 사진</div>}
         >
-          <div id="upload-img-placeholder">
-            <img src="/images/icons/camera.png" />
-            <span>이미지를 업로드를해주세요.</span>
-          </div>
+          <Upload
+            name="image"
+            action={`${API_URL}/image`}
+            listType="picture"
+            showUploadList={false}
+            onChange={onChangeImage}
+          >
+            {imageUrl ? (
+              <img id="upload-img" src={`${API_URL}/${imageUrl}`} />
+            ) : (
+              <div id="upload-img-placeholder">
+                <img src="/images/icons/camera.png" />
+                <span>이미지를 업로드를해주세요.</span>
+              </div>
+            )}
+          </Upload>
         </Form.Item>
         <Divider />
         <Form.Item
